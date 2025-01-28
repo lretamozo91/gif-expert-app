f73295bbcf890029a98c236a7356b6d425c38ff6
Luis <lretamozo91@gmail.com>
2025-01-28 16:20:11 -0300

    falta realizar el test para GifExpertApp

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react"
+
+describe('Pruebas en <GifExpertApp/>', () => {
+
+  test('should ', () => {
+    
+    render( <GifExpertApp /> )
+    screen.debug();
+    
+  })
+  
+})
